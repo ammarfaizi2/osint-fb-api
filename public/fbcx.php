a2c2b20165e33e780c1f2922241575cbdae025ca
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-31 12:37:30 +0700

    Fix

--- public/fbcx.php
+++ public/fbcx.php
@@ -37,34 +37,34 @@ try {
 			"profile_picture" => null,
 			"profile_url" => null,
 			"extended_info" => [
-			// 	"work" => [],
-			// 	"education" => [],
-			// 	"living" => [],
-			// 	"contact_info" => [
-			// 		"mobile" => [],
-			// 		"address" => [],
-			// 		"mobile" => null,
-			// 		"address" => [],
-			// 		"facebook" => null,
-			// 		"github" => null,
-			// 		"youtube" => null,
-			// 		"yahoo!_messenger" => null,
-			// 		"line" => null,
-			// 		"bbm" => null,
-			// 		"instagram" => null,
-			// 		"twitter" => null,
-			// 		"websites" => [],
-			// 		"email" => [],
-			// 	]
-			// ],
-			// "basic_info" => [
-			// 	"birthday" => null,
-			// 	"gender" => null,
-			// 	"interested_in" => null,
-			// 	"languages" => null,
-			// 	"religious_views" => null,
-			// 	"political_views" => null,
-			// ]
+				"work" => [],
+				"education" => [],
+				"living" => [],
+				"contact_info" => [
+					"mobile" => [],
+					"address" => [],
+					"mobile" => null,
+					"address" => [],
+					"facebook" => null,
+					"github" => null,
+					"youtube" => null,
+					"yahoo!_messenger" => null,
+					"line" => null,
+					"bbm" => null,
+					"instagram" => null,
+					"twitter" => null,
+					"websites" => [],
+					"email" => [],
+				]
+			],
+			"basic_info" => [
+				"birthday" => null,
+				"gender" => null,
+				"interested_in" => null,
+				"languages" => null,
+				"religious_views" => null,
+				"political_views" => null,
+			]
 		],
 		"user_posts" => []
 	];
