0671202c2d37dd7a6677558d3430af34fb1eac55
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-31 12:30:47 +0700

    Update json keys

--- public/fbcx.php
+++ public/fbcx.php
@@ -36,7 +36,35 @@ try {
 			"name" => null,
 			"profile_picture" => null,
 			"profile_url" => null,
-			"extended_info" => []
+			"extended_info" => [
+				"work" => [],
+				"education" => [],
+				"living" => [],
+				"contact_info" => [
+					"mobile" => [],
+					"address" => [],
+					"mobile" => null,
+					"address" => [],
+					"facebook" => null,
+					"github" => null,
+					"youtube" => null,
+					"yahoo!_messenger" => null,
+					"line" => null,
+					"bbm" => null,
+					"instagram" => null,
+					"twitter" => null,
+					"websites" => [],
+					"email" => [],
+				]
+			],
+			"basic_info" => [
+				"birthday" => null,
+				"gender" => null,
+				"interested_in" => null,
+				"languages" => null,
+				"religious_views" => null,
+				"political_views" => null,
+			]
 		],
 		"user_posts" => []
 	];
